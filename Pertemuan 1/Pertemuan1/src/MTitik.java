19aee6efd1ea03958878a977a72a6eb26e6ed7d7
dianberlian <dianberlianhutasoit@gmail.com>
2026-02-26 14:22:34 +0700

    Pertemuan 1 clear

--- Pertemuan 1/Pertemuan1/src/MTitik.java
+++ Pertemuan 1/Pertemuan1/src/MTitik.java
@@ -12,5 +12,11 @@ public class MTitik {
         T1.printTitik();
         T1.geser(3, 4);
         T1.printTitik();
+
+        Titik T2 = T1;
+        T2.printTitik();
+        T1.setAbsis(10);
+        T1.setOrdinat(10);
+        T2.printTitik();
     }
 }
