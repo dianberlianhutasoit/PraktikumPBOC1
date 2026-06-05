78f0387ab0d0bba565cea99ec2630194bf92e1bb
dianberlian <dianberlianhutasoit@gmail.com>
2026-06-05 11:42:45 +0700

    Pertemuan 12

---
+++ Pertemuan12/src/LambdaList.java
@@ -0,0 +1,21 @@
+/* Nama File    : LambdaList.java
+Deskripsi       : Implementasi lambda pada List, digunakan sebagai parameter pada method.
+Pembuat         : Dian Berlian Hutasoit
+Tanggal         : 7 Mei 2026
+LAB             : C1
+*/
+
+import java.util.ArrayList;
+
+public class LambdaList {
+    public static void main(String[] args) {
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Didi");
+
+        // Lambda digunakan sebagai parameter
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
+}
